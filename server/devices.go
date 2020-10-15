5867eeb0e733f6f103eebc75e26aad2a954bbf09
Shan Sikdar <ssikdar1@bu.edu>
2020-10-15 14:00:45 -0400

    Add unit tests for GET

--- server/devices.go
+++ server/devices.go
@@ -10,111 +10,75 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-/***
-    URI: /client/devices
-    paths:
-        GET:
-            responses:
-                200:
-                    description: list of all devices being managed
-***/
+// TODO This API could be problematic when number of devices is large
+// Probably need to implement paginating, and/or skip if using cassandra
 func (s *Server) DevicesHandler(w http.ResponseWriter, req *http.Request) {
 
 	var msg ServerMsg
 	var statusCode int
+	var devices clientdevices.Devices
+	var err error
 
 	defer func() {
 		// Write response
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(statusCode)
-		json.NewEncoder(w).Encode(msg)
+		if statusCode == http.StatusOK {
+			json.NewEncoder(w).Encode(devices)
+		} else {
+			json.NewEncoder(w).Encode(msg)
+		}
 	}()
 
 	switch req.Method {
 	case "GET":
 		// List information on all devices
-		devices, err := s.DeivcesClient.GetAllDevices()
+		devices, err = s.DeivcesClient.GetAllDevices()
 		if err != nil {
 			statusCode = http.StatusInternalServerError
 			msg = ServerMsg{Message: "Error getting devices"}
 			log.Error("Error getting devices")
 			return
 		}
-		json.NewEncoder(w).Encode(devices)
-
+		statusCode = http.StatusOK
 	default:
 		// Give an error message.
-		msg := ServerMsg{Message: "HTTP Method not supported"}
-		json.NewEncoder(w).Encode(msg)
+		statusCode = http.StatusMethodNotAllowed
+		msg = ServerMsg{Message: "HTTP Method not supported"}
 	}
 }
 
-/***
-    URI: /client/device
-    paths:
-        GET:
-            query parameters:
-                device: name of device to get info for
-            responses:
-                200:
-                    description: return information on device
-	POST:
-            parameters:
-                name string : name of device
-                mac_address string : mac_address of device
-                ip_address  string: ip of device
-                platform string: platform
-            responses:
-                200:
-                    description: device entry created
-	PUT:
-            parameters:
-                name string : name of device
-                mac_address string : mac_address of device
-                ip_address  string: ip of device
-                platform string: platform
-            responses:
-                200:
-                    description: device entry updated based on information in body
-	DELETE:
-            query parameters:
-                device: name of device to get info for
-            responses:
-                200:
-                    description: device removed
-***/
 func (s *Server) DeviceHandler(w http.ResponseWriter, req *http.Request) {
 
-	var msg ServerMsg
+	var msg []byte
 	var statusCode int
 
 	defer func() {
 		// Write response
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(statusCode)
-		json.NewEncoder(w).Encode(msg)
+		w.Write(msg)
 	}()
 
 	switch req.Method {
 	case "GET":
 		// List information on a specific device
-		urlPar, _ := url.Parse(req.RequestURI)
-		qmap, _ := url.ParseQuery(urlPar.RawQuery)
-		ret, err := s.DeivcesClient.FindDeviceByName(qmap["device"][0])
+		qmap, _ := url.ParseQuery(req.URL.RawQuery)
+		ret, err := s.DeivcesClient.FindDeviceByName(qmap.Get("device"))
 		if err != nil {
 			statusCode = http.StatusAccepted
-			msg = ServerMsg{Message: "Device not found"}
+			msg, _ = json.Marshal(ServerMsg{Message: "Device not found"})
 			return
 		}
 		statusCode = http.StatusOK
-		json.NewEncoder(w).Encode(ret)
+		msg, _ = json.Marshal(ret)
 	case "POST":
 		// Add a new device.
 		newDevice, err := clientdevices.JsonReq2Device(req)
 
 		if err != nil {
 			statusCode = http.StatusBadRequest
-			msg = ServerMsg{Message: fmt.Sprintf("Error decoding json: %s", err)}
+			msg, _ = json.Marshal(ServerMsg{Message: fmt.Sprintf("Error decoding json: %s", err)})
 			return
 		}
 		err = s.DeivcesClient.AddDevice(newDevice)
@@ -124,18 +88,18 @@ func (s *Server) DeviceHandler(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		statusCode = http.StatusOK
-		msg = ServerMsg{Message: "Adding new device successful"}
+		msg, _ = json.Marshal(ServerMsg{Message: "Adding new device successful"})
 	case "PUT":
 		// Update an existing record.
 		newDevice, err := clientdevices.JsonReq2Device(req)
 		if err != nil {
 			statusCode = http.StatusBadRequest
-			msg = ServerMsg{Message: fmt.Sprintf("Error decoding json: %s", err)}
+			msg, _ = json.Marshal(ServerMsg{Message: fmt.Sprintf("Error decoding json: %s", err)})
 			return
 		}
 
 		if newDevice.Name == "" {
-			msg = ServerMsg{Message: "You must specify name when trying to update device"}
+			msg, _ = json.Marshal(ServerMsg{Message: "You must specify name when trying to update device"})
 			statusCode = http.StatusBadRequest
 			return
 		}
@@ -146,7 +110,7 @@ func (s *Server) DeviceHandler(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		statusCode = http.StatusOK
-		msg = ServerMsg{Message: "Update successful"}
+		msg, _ = json.Marshal(ServerMsg{Message: "Update successful"})
 	case "DELETE":
 		// Remove the record.
 		urlPar, _ := url.Parse(req.RequestURI)
@@ -155,13 +119,13 @@ func (s *Server) DeviceHandler(w http.ResponseWriter, req *http.Request) {
 
 		if err != nil {
 			statusCode = http.StatusBadRequest
-			msg = ServerMsg{Message: fmt.Sprintf("Couldn't Delete device: %s", qmap["device"][0])}
+			msg, _ = json.Marshal(ServerMsg{Message: fmt.Sprintf("Couldn't Delete device: %s", qmap["device"][0])})
 			return
 		}
 		statusCode = http.StatusOK
-		msg = ServerMsg{Message: "Delete successful"}
+		msg, _ = json.Marshal(ServerMsg{Message: "Delete successful"})
 	default:
-		msg = ServerMsg{Message: "HTTP Method not supported"}
 		statusCode = http.StatusMethodNotAllowed
+		msg, _ = json.Marshal(ServerMsg{Message: "HTTP Method not supported"})
 	}
 }
--- server/devices_test.go
+++ server/devices_test.go
@@ -7,41 +7,176 @@ import (
 	"testing"
 
 	main "github.com/rgdaddio/gobotics/server"
+	cd "github.com/rgdaddio/gobotics/utils/clientdevices"
 	mock "github.com/rgdaddio/gobotics/utils/clientdevices/mock"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDevicesHandler(t *testing.T) {
-
-	s := main.Server{}
-	dc, _ := mock.NewMockClient()
-	s.DeivcesClient = dc
-	serverHandlerFunc := s.DevicesHandler
-
-	endpointUri := "/client/devices"
-
-	req, err := http.NewRequest("GET", endpointUri, nil)
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		name                 string
+		requestMethod        string
+		devicesToAdd         cd.Devices
+		expectedResponseCode int
+		expectedResponse     string
+	}{
+		{
+			name:                 "test when there are no devices",
+			requestMethod:        "GET",
+			expectedResponseCode: http.StatusOK,
+			expectedResponse:     `[]`,
+		},
+		{
+			name:          "Add one device and test get it",
+			requestMethod: "GET",
+			devicesToAdd: []cd.Device{
+				cd.Device{
+					Name:     "foo",
+					Ip:       "1.1.1.1",
+					Mac:      "::0",
+					Platform: "test",
+				},
+			},
+			expectedResponseCode: http.StatusOK,
+			expectedResponse:     `[{"name":"foo","platform":"test","mac_address":"::0","ip_address":"1.1.1.1"}]`,
+		},
+		{
+			name:                 "test unsupported method",
+			requestMethod:        "POST",
+			expectedResponseCode: http.StatusMethodNotAllowed,
+			expectedResponse:     `{"message":"HTTP Method not supported"}`,
+		},
 	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(serverHandlerFunc)
 
-	handler.ServeHTTP(rr, req)
-	status := rr.Code
-	assert.Equal(t, http.StatusOK, status)
-	assert.Equal(t, `{"message":"live"}`, strings.TrimSuffix(rr.Body.String(), "\n"))
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
 
-	// TODO Unsported method
-	req, err = http.NewRequest("PUT", endpointUri, nil)
-	if err != nil {
-		t.Fatal(err)
+			s := main.Server{}
+			dc, _ := mock.NewMockClient()
+			s.DeivcesClient = dc
+			serverHandlerFunc := s.DevicesHandler
+			endpointUri := "/client/devices"
+
+			if len(tc.devicesToAdd) > 0 {
+				for _, device := range tc.devicesToAdd {
+					s.DeivcesClient.AddDevice(device)
+				}
+			}
+
+			req, err := http.NewRequest(tc.requestMethod, endpointUri, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(serverHandlerFunc)
+			handler.ServeHTTP(rr, req)
+			status := rr.Code
+			assert.Equal(t, tc.expectedResponseCode, status)
+			assert.Equal(t, tc.expectedResponse, strings.TrimSuffix(rr.Body.String(), "\n"))
+
+		})
 	}
-	rr = httptest.NewRecorder()
-	handler = http.HandlerFunc(serverHandlerFunc)
+}
 
-	handler.ServeHTTP(rr, req)
-	status = rr.Code
-	assert.Equal(t, http.StatusMethodNotAllowed, status)
-	assert.Equal(t, `{"message":"live"}`, strings.TrimSuffix(rr.Body.String(), "\n"))
+func TestDeviceHandlerGET(t *testing.T) {
+	testCases := []struct {
+		name                 string
+		devicesToAdd         cd.Devices
+		deviceNameToFind     string
+		expectedResponseCode int
+		expectedResponse     string
+	}{
+		{
+			name:                 "no devices added nothing is returned",
+			expectedResponseCode: http.StatusAccepted,
+			expectedResponse:     `{"message":"Device not found"}`,
+		},
+		{
+			name: "add one device try and find it",
+			devicesToAdd: []cd.Device{
+				cd.Device{
+					Name:     "foo",
+					Ip:       "1.1.1.1",
+					Mac:      "::0",
+					Platform: "test",
+				},
+			},
+			deviceNameToFind:     "foo",
+			expectedResponseCode: http.StatusOK,
+			expectedResponse:     `{"name":"foo","platform":"test","mac_address":"::0","ip_address":"1.1.1.1"}`,
+		},
+		{
+			name: "add one device try and find a device that doesnt exist",
+			devicesToAdd: []cd.Device{
+				cd.Device{
+					Name:     "foo",
+					Ip:       "1.1.1.1",
+					Mac:      "::0",
+					Platform: "test",
+				},
+			},
+			deviceNameToFind:     "bar",
+			expectedResponseCode: http.StatusAccepted,
+			expectedResponse:     `{"message":"Device not found"}`,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			s := main.Server{}
+			dc, _ := mock.NewMockClient()
+			s.DeivcesClient = dc
+			serverHandlerFunc := s.DeviceHandler
+			endpointUri := "/client/devices"
+
+			if len(tc.devicesToAdd) > 0 {
+				for _, device := range tc.devicesToAdd {
+					s.DeivcesClient.AddDevice(device)
+				}
+			}
+
+			req, err := http.NewRequest("GET", endpointUri, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tc.deviceNameToFind != "" {
+				q := req.URL.Query()
+				q.Add("device", tc.deviceNameToFind)
+				req.URL.RawQuery = q.Encode()
+			}
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(serverHandlerFunc)
+			handler.ServeHTTP(rr, req)
+			status := rr.Code
+			assert.Equal(t, tc.expectedResponseCode, status)
+			assert.Equal(t, tc.expectedResponse, strings.TrimSuffix(rr.Body.String(), "\n"))
+
+		})
+	}
+}
+
+func TestDeviceHandlerPOST(t *testing.T) {
+
+	// no body
+
+	// inccorect json
+
+	// add device
+
+	// adding same device again
+	// what is the behavior here?
+	// uuid?
+
+}
+
+func TestDeviceHandlerPUT(t *testing.T) {
+
+}
+
+func TestDeviceHandlerDELETE(t *testing.T) {
+
 }
